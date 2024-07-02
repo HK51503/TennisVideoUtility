8b70056d641eef73db0985c330369ea34e2643fd
HK <46559811+HK51503@users.noreply.github.com>
2024-07-03 02:18:47 +0900

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-list = ["s1", "s2", "d1", "d2"]
-for i in range(len(list)):
-    value = list[i]
-    if value[0] == "s":
-        print("singles:" + value)
-    else:
-        print("doubles:" + value)
