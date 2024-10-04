9d34152eb95b76486bba83317f9a34bd07d79cba
HK <46559811+HK51503@users.noreply.github.com>
2024-10-04 23:30:40 +0900

    Add google login
    unfinished

--- window_edit_settings.py
+++ window_edit_settings.py
@@ -1,9 +1,10 @@
 from PySide6.QtWidgets import (
     QTabWidget, QWidget, QRadioButton, QLabel, QGroupBox, QVBoxLayout, QGridLayout,
-    QButtonGroup, QFrame, QComboBox, QSpacerItem, QSizePolicy
+    QButtonGroup, QFrame, QComboBox, QSpacerItem, QSizePolicy, QPushButton
 )
 from PySide6.QtCore import Qt
 import functions_settings_config as settings
+import tool_youtube_upload
 
 
 class EditSettingsWindow(QWidget):
@@ -15,6 +16,7 @@ class EditSettingsWindow(QWidget):
         main_v_layout = QVBoxLayout()
         tab_widget.addTab(VideoSettingsTab(), self.tr("動画設定"))
         tab_widget.addTab(GeneralSettingsTab(), self.tr("一般設定"))
+        tab_widget.addTab(YouTubeSettingsTab(), self.tr("YouTube設定"))
         main_v_layout.addWidget(tab_widget)
         self.setLayout(main_v_layout)
 
@@ -28,24 +30,6 @@ class VideoSettingsTab(QWidget):
         video_setting_groupbox = QGroupBox()
         video_setting_layout = QGridLayout()
 
-        # youtube upload settings
-        self.youtube_upload_setting_button_group = QButtonGroup()
-        youtube_upload_true_button = QRadioButton(self.tr("する（Coming Soon!）"))
-        youtube_upload_false_button = QRadioButton(self.tr("しない"))
-        self.youtube_upload_setting_button_group.addButton(youtube_upload_true_button, 1)
-        self.youtube_upload_setting_button_group.addButton(youtube_upload_false_button, 2)
-        self.youtube_upload_setting_button_group.buttonClicked.connect(self.youtube_upload_setting_button_clicked)
-        if settings.read_value("video_settings", "youtube_upload") == "True":
-            youtube_upload_true_button.setChecked(True)
-        else:
-            youtube_upload_false_button.setChecked(True)
-
-        video_setting_layout.addWidget(QLabel(self.tr("YouTubeにアップロード")), 0, 0)
-        video_setting_layout.addWidget(youtube_upload_true_button, 0, 1)
-        video_setting_layout.addWidget(youtube_upload_false_button, 1, 1)
-
-        youtube_upload_true_button.setEnabled(False)
-
         # stitch videos using ffmpeg setting
         self.stitch_videos_setting_button_group = QButtonGroup()
         stitch_videos_true_button = QRadioButton(self.tr("する"))
@@ -58,9 +42,9 @@ class VideoSettingsTab(QWidget):
         else:
             stitch_videos_false_button.setChecked(True)
 
-        video_setting_layout.addWidget(QLabel(self.tr("動画を結合")), 3, 0)
-        video_setting_layout.addWidget(stitch_videos_true_button, 3, 1)
-        video_setting_layout.addWidget(stitch_videos_false_button, 4, 1)
+        video_setting_layout.addWidget(QLabel(self.tr("動画を結合")), 0, 0)
+        video_setting_layout.addWidget(stitch_videos_true_button, 0, 1)
+        video_setting_layout.addWidget(stitch_videos_false_button, 1, 1)
 
         # keep original file setting
         self.keep_original_setting_button_group = QButtonGroup()
@@ -74,9 +58,9 @@ class VideoSettingsTab(QWidget):
         else:
             keep_original_false_button.setChecked(True)
 
-        video_setting_layout.addWidget(QLabel(self.tr("元ファイルを")), 6, 0)
-        video_setting_layout.addWidget(keep_original_true_button, 6, 1)
-        video_setting_layout.addWidget(keep_original_false_button, 7, 1)
+        video_setting_layout.addWidget(QLabel(self.tr("元ファイルを")), 3, 0)
+        video_setting_layout.addWidget(keep_original_true_button, 3, 1)
+        video_setting_layout.addWidget(keep_original_false_button, 4, 1)
 
         # add separator line
         line = QFrame()
@@ -84,16 +68,9 @@ class VideoSettingsTab(QWidget):
         line.setLineWidth(0)
         line.setMidLineWidth(0)
         video_setting_layout.addWidget(line, 2, 0, 1, 2)
-        video_setting_layout.addWidget(line, 5, 0, 1, 2)
 
         video_setting_groupbox.setLayout(video_setting_layout)
         main_v_layout.addWidget(video_setting_groupbox, alignment=Qt.AlignTop)
-
-    def youtube_upload_setting_button_clicked(self):
-        if self.youtube_upload_setting_button_group.checkedId() == 1:
-            settings.set_value("video_settings", "youtube_upload", "True")
-        else:
-            settings.set_value("video_settings", "youtube_upload", "False")
 
     def stitch_videos_setting_button_clicked(self):
         if self.stitch_videos_setting_button_group.checkedId() == 1:
@@ -140,3 +117,47 @@ class GeneralSettingsTab(QWidget):
             settings.set_value("general_settings", "language", "en")
 
         self.language_change_restart_label.show()
+
+class YouTubeSettingsTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        main_v_layout = QVBoxLayout()
+        self.setLayout(main_v_layout)
+
+        youtube_setting_groupbox = QGroupBox()
+        youtube_setting_layout = QGridLayout()
+
+        # youtube upload settings
+        self.youtube_upload_setting_button_group = QButtonGroup()
+        youtube_upload_true_button = QRadioButton(self.tr("する（Coming Soon!）"))
+        youtube_upload_false_button = QRadioButton(self.tr("しない"))
+        self.youtube_upload_setting_button_group.addButton(youtube_upload_true_button, 1)
+        self.youtube_upload_setting_button_group.addButton(youtube_upload_false_button, 2)
+        self.youtube_upload_setting_button_group.buttonClicked.connect(self.youtube_upload_setting_button_clicked)
+        if settings.read_value("video_settings", "youtube_upload") == "True":
+            youtube_upload_true_button.setChecked(True)
+        else:
+            youtube_upload_false_button.setChecked(True)
+
+        youtube_setting_layout.addWidget(QLabel(self.tr("YouTubeにアップロード")), 0, 0)
+        youtube_setting_layout.addWidget(youtube_upload_true_button, 0, 1)
+        youtube_setting_layout.addWidget(youtube_upload_false_button, 0, 2)
+
+        youtube_login_label = QLabel(self.tr("Googleアカウント"))
+        youtube_login_button = QPushButton()
+        youtube_login_button.clicked.connect(self.youtube_login_button_clicked)
+
+        youtube_setting_layout.addWidget(youtube_login_label, 1, 0)
+        youtube_setting_layout.addWidget(youtube_login_button, 1, 1)
+
+        youtube_setting_groupbox.setLayout(youtube_setting_layout)
+        main_v_layout.addWidget(youtube_setting_groupbox, alignment=Qt.AlignTop)
+
+    def youtube_upload_setting_button_clicked(self):
+        if self.youtube_upload_setting_button_group.checkedId() == 1:
+            settings.set_value("video_settings", "youtube_upload", "True")
+        else:
+            settings.set_value("video_settings", "youtube_upload", "False")
+
+    def youtube_login_button_clicked(self):
+        tool_youtube_upload.get_authenticated_service()
